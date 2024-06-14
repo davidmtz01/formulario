2444f5640c637819b6c13ae1a5da8ca3e58a27b6
davidmtz01 <95955080+davidmtz01@users.noreply.github.com>
2024-06-14 10:32:37 -0600

    Update Formulario.routes.js

--- src/Routes/Formulario.routes.js
+++ src/Routes/Formulario.routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { getFormulario, saveFormulario, updateFormulario, deleteFormulario } from '../Controllers/FormularioController.js';
 
 const rutas = Router();
+const upload = multer();
+
+router.post('/api/formulario', upload.none(), saveFormulario);
 
 rutas.get('/formulario', getFormulario);
 rutas.get('/formulario/:id', getFormulario);
-rutas.post('/formulario', saveFormulario);
+//rutas.post('/formulario', saveFormulario);
 rutas.put('/formulario/:id', updateFormulario);
 rutas.delete('/formulario/:id', deleteFormulario);
 
